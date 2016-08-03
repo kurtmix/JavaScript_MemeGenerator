9abdc5ed4fc7c7f63299beec0cc402bf3379e37d
kurtmix <kurtmix123@gmail.com>
2016-08-03 15:59:34 -0400

    Fixed Linkage and Js

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-
-console.log ("hey");
+$(document).ready(function () {
+  var x = ("<img src = \"" + Math.floor(1 + Math.random() * 7) + ".jpg\"/>" );
+  var hidingSomething = true;
+  $(x).hide();
+  
+  $('#click-div').click(function() {
+    hidingSomething = !hidingSomething;
+    
+    $(x).slideToggle(1000, function() {
+      if (hidingSomething) {
+        $('#click-div').text("I'm hiding something...");
+      } else {
+        $('#click-div').append(x);      
+      }
+                                            });
+                                     });
+});
